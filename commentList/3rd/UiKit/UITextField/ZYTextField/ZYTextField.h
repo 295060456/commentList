5306711045d76c9197d2c148845ec0c84ca3da40
Jobs <>
2020-09-04 16:05:10 +0700

    no message

--- commentList/3rd/UiKit/UITextField/ZYTextField/ZYTextField.h
+++ commentList/3rd/UiKit/UITextField/ZYTextField/ZYTextField.h
@@ -5,4 +5,16 @@
 
 @property(nonatomic,assign)BOOL isInputting;//是否正在输入(键盘弹起中)
 
+@property(nonatomic,strong)UIFont *ZYtextFont;//字体大小
+@property(nonatomic,strong)UIColor *ZYtextColor;//文本颜色
+@property(nonatomic,strong)UIColor *ZYtintColor;//光标颜色
+//
+@property(nonatomic,strong)UIColor *ZYplaceholderLabelTextColor_1;//占位文字颜色 失去焦点
+@property(nonatomic,strong)UIColor *ZYplaceholderLabelTextColor_2;//占位文字颜色 聚焦状态
+@property(nonatomic,strong)UIFont *ZYplaceholderLabelFont_1;//占位文字字体 失去焦点
+@property(nonatomic,strong)UIFont *ZYplaceholderLabelFont_2;//占位文字字体 聚焦状态
+
+@property(nonatomic,assign)CGFloat offset;
+
+
 @end
--- commentList/3rd/UiKit/UITextField/ZYTextField/ZYTextField.m
+++ commentList/3rd/UiKit/UITextField/ZYTextField/ZYTextField.m
@@ -1,30 +1,31 @@
 
 #import "ZYTextField.h"
 
+@interface ZYTextField ()
+
+@property(nonatomic,assign)BOOL isOk;
+
+@end
+
 @implementation ZYTextField
-//通过代码创建
-- (instancetype)initWithFrame:(CGRect)frame{
-    if (self = [super initWithFrame:frame]) {
+
+-(void)drawRect:(CGRect)rect{
+    [super drawRect:rect];
+    if (!self.isOk) {
         [self setUpUI];
-    }return self;
-}
-//通过xib创建
--(void)awakeFromNib{
-    [super awakeFromNib];
-    [self setUpUI];
+    }
 }
 // 重写此方法
--(void)drawPlaceholderInRect:(CGRect)rect {
-    // 计算占位文字的 Size
-    CGSize placeholderSize = [self.placeholder sizeWithAttributes:
-                              @{NSFontAttributeName : self.font}];
-    [self.placeholder drawInRect:CGRectMake(0,
-                                            (rect.size.height - placeholderSize.height) / 2,
-                                            rect.size.width,
-                                            rect.size.height)
-                  withAttributes:@{NSForegroundColorAttributeName : kWhiteColor,
-                                   NSFontAttributeName : self.font}];
-}
+//-(void)drawPlaceholderInRect:(CGRect)rect {
+//    // 计算占位文字的 Size
+//    CGSize placeholderSize = [self.placeholder sizeWithAttributes: @{NSFontAttributeName : self.font}];
+//    [self.placeholder drawInRect:CGRectMake(0,
+//                                            (rect.size.height - placeholderSize.height) / 2,
+//                                            rect.size.width,
+//                                            rect.size.height)
+//                  withAttributes:@{NSForegroundColorAttributeName : kWhiteColor,
+//                                   NSFontAttributeName : self.font}];
+//}
 
 - (void)setUpUI{
 //    设置border
@@ -34,25 +35,20 @@
 //    self.layer.borderColor = [UIColor blackColor].CGColor;
 //    self.layer.borderWidth = 1;
     //字体大小
-    self.font = [UIFont fontWithName:@"PingFangSC"
-                                size: 17];
+    self.font = self.ZYtextFont;
     //字体颜色
-    self.textColor = RGBCOLOR(77,
-                              150,
-                              132);
+    self.textColor = self.ZYtextColor;
     //光标颜色
-    self.tintColor = self.textColor;
+    self.tintColor = self.ZYtintColor;
     //占位符的颜色和大小
     UILabel *placeholderLabel = object_getIvar(self,
-                                               class_getInstanceVariable([UITextField class],
+                                               class_getInstanceVariable(UITextField.class,
                                                                          "_placeholderLabel"));
-    placeholderLabel.textColor = RGBCOLOR(167,
-                                          167,
-                                          167);
-    placeholderLabel.font = [UIFont boldSystemFontOfSize:15];
-    
+    placeholderLabel.textColor = self.ZYplaceholderLabelTextColor_1;
+    placeholderLabel.font = self.ZYplaceholderLabelFont_1;
     // 不成为第一响应者
     [self resignFirstResponder];
+    self.isOk = YES;
 }
 /**
  * 当前文本框聚焦时就会调用
@@ -60,9 +56,10 @@
 - (BOOL)becomeFirstResponder{
     // 修改占位文字颜色
     UILabel *placeholderLabel = object_getIvar(self,
-                                               class_getInstanceVariable([UITextField class],
+                                               class_getInstanceVariable(UITextField.class,
                                                                          "_placeholderLabel"));
-    placeholderLabel.textColor = self.textColor;
+    placeholderLabel.textColor = self.ZYplaceholderLabelTextColor_2;
+    placeholderLabel.font = self.ZYplaceholderLabelFont_2;
     return [super becomeFirstResponder];
 }
 /**
@@ -71,36 +68,93 @@
 - (BOOL)resignFirstResponder{
     // 修改占位文字颜色
     UILabel *placeholderLabel = object_getIvar(self,
-                                               class_getInstanceVariable([UITextField class],
+                                               class_getInstanceVariable(UITextField.class,
                                                                          "_placeholderLabel"));
-    placeholderLabel.textColor = COLOR_RGB(131, 145, 175, 1);
+    placeholderLabel.textColor = self.ZYplaceholderLabelTextColor_1;
+    placeholderLabel.font = self.ZYplaceholderLabelFont_1;
     return [super resignFirstResponder];
+}
+#pragma mark —— 重写父类方法
+-(CGRect)leftViewRectForBounds:(CGRect)bounds{
+    CGRect iconRect = [super leftViewRectForBounds:bounds];
+    iconRect.origin.x += 5;
+    return iconRect;
 }
 //控制placeHolder的位置
 -(CGRect)placeholderRectForBounds:(CGRect)bounds{
-    CGRect inset = CGRectMake(bounds.origin.x + 15,
+    CGRect inset = CGRectMake(bounds.origin.x + self.offset,
                               bounds.origin.y,
-                              bounds.size.width -15,
+                              bounds.size.width - self.offset,
                               bounds.size.height);
     return inset;
 }
-
 //控制显示文本的位置
 -(CGRect)textRectForBounds:(CGRect)bounds{
-    CGRect inset = CGRectMake(bounds.origin.x + 15,
+    CGRect inset = CGRectMake(bounds.origin.x + self.offset,
                               bounds.origin.y,
-                              bounds.size.width - 15,
+                              bounds.size.width - self.offset,
                               bounds.size.height);
     return inset;
 }
-
 //控制编辑文本的位置
 -(CGRect)editingRectForBounds:(CGRect)bounds{
-    CGRect inset = CGRectMake(bounds.origin.x + 15,
+    CGRect inset = CGRectMake(bounds.origin.x + self.offset,
                               bounds.origin.y,
-                              bounds.size.width - 15,
+                              bounds.size.width - self.offset,
                               bounds.size.height);
     return inset;
 }
+#pragma mark —— lazyLoad
+-(UIFont *)ZYtextFont{
+    if (!_ZYtextFont) {
+        _ZYtextFont = [UIFont systemFontOfSize:17
+                                        weight:UIFontWeightRegular];
+    }return _ZYtextFont;
+}
+
+-(UIColor *)ZYtextColor{
+    if (!_ZYtextColor) {
+        _ZYtextColor = RGBCOLOR(77,
+                                150,
+                                132);
+    }return _ZYtextColor;
+}
+
+-(UIColor *)ZYtintColor{
+    if (!_ZYtintColor) {
+        _ZYtintColor = self.textColor;
+    }return _ZYtintColor;
+}
+
+-(UIColor *)ZYplaceholderLabelTextColor_1{
+    if (!_ZYplaceholderLabelTextColor_1) {
+        _ZYplaceholderLabelTextColor_1 = self.textColor;
+    }return _ZYplaceholderLabelTextColor_1;
+}
+
+-(UIColor *)ZYplaceholderLabelTextColor_2{
+    if (!_ZYplaceholderLabelTextColor_2) {
+        _ZYplaceholderLabelTextColor_2 = self.textColor;
+    }return _ZYplaceholderLabelTextColor_2;
+}
+
+-(UIFont *)ZYplaceholderLabelFont_1{
+    if (!_ZYplaceholderLabelFont_1) {
+        _ZYplaceholderLabelFont_1 = self.font;
+    }return _ZYplaceholderLabelFont_1;
+}
+
+-(UIFont *)ZYplaceholderLabelFont_2{
+    if (!_ZYplaceholderLabelFont_2) {
+        _ZYplaceholderLabelFont_2 = self.font;
+    }return _ZYplaceholderLabelFont_2;
+}
+
+-(CGFloat)offset{
+    if (_offset == 0) {
+        _offset = 30;
+    }return _offset;
+}
+
 
 @end
