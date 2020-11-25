be77505bd89c85c4117bd083f8ab47d3bc1bac89
Jobs <lg295060456@gmail.com>
2020-11-25 17:12:36 +0700

    no message

--- commentList/3rd/UiKit/UILabel/UILabel+RichText.m
+++ commentList/3rd/UiKit/UILabel/UILabel+RichText.m
@@ -5,7 +5,7 @@
 //  Created by Jobs on 2020/11/4.
 //
 
-#import "UILabel+RichText.h"
+#import "NSObject+RichText.h"
 
 @implementation RichLabelFontModel
 
@@ -63,9 +63,9 @@
 
 @end
 
-@implementation UILabel (RichText)
+@implementation NSObject (RichText)
 
--(NSAttributedString *)makeRichTextWithDataConfigMutArr:(NSArray <RichLabelDataStringsModel *>*_Nonnull)richTextDataConfigMutArr{
++(NSAttributedString *)makeRichTextWithDataConfigMutArr:(NSArray <RichLabelDataStringsModel *>*_Nonnull)richTextDataConfigMutArr{
     
     NSString *resultString = @"";
     
